8cbbccaf8af9ab156b529b79b69f1412207bb59e
jimcreel <jim.creel@gmail.com>
2023-01-10 14:46:46 -0800

    comments and prettier

--- magic-res.js
+++ magic-res.js
@@ -1,3 +1,9 @@
+let dlrUrl =
+	"https://disneyland.disney.go.com/passes/blockout-dates/api/get-availability/?product-types=inspire-key-pass,believe-key-pass,enchant-key-pass,imagine-key-pass,dream-key-pass&destinationId=DLR&numMonths=14";
+
+let wdwUrl =
+	"https://disneyworld.disney.go.com/passes/blockout-dates/api/get-availability/?product-types=disney-incredi-pass,disney-sorcerer-pass,disney-pirate-pass,disney-pixie-dust-pass&destinationId=WDW&numMonths=13";
+
 $(document).ready(function () {
 	var date_input = $('input[name="date"]'); //our date input has the name "date"
 	var container =
@@ -15,6 +21,7 @@ $(document).ready(function () {
 	date_input.datepicker(options);
 });
 
+/*hide and reveal contextual menus depending on resort*/
 function testResort(val) {
 	if (val == "DLR") {
 		document.getElementById("DLRdrops").classList.remove("hidden");
@@ -28,6 +35,7 @@ function testResort(val) {
 	}
 }
 
+/* hide and remove contextual menus depending on resort */
 function weekResortCheck(val) {
 	if (val == "DLR") {
 		document.getElementById("weekDLR").classList.remove("hidden");
@@ -41,6 +49,7 @@ function weekResortCheck(val) {
 	}
 }
 
+/* aggregate data from the form to send to the park checker */
 function submitForm() {
 	resort = document.querySelector("#selectResort").value;
 
@@ -60,6 +69,9 @@ function submitForm() {
 
 	getResortData(url, pass, park, parkDate);
 }
+
+/* constructor function for user objects */
+
 function userData(resort, pass, park, parkDate) {
 	this.resort = resort;
 	this.pass = pass;
@@ -67,30 +79,26 @@ function userData(resort, pass, park, parkDate) {
 	this.date = parkDate;
 }
 
-let dlrUrl =
-	"https://disneyland.disney.go.com/passes/blockout-dates/api/get-availability/?product-types=inspire-key-pass,believe-key-pass,enchant-key-pass,imagine-key-pass,dream-key-pass&destinationId=DLR&numMonths=14";
-
-let wdwUrl =
-	"https://disneyworld.disney.go.com/passes/blockout-dates/api/get-availability/?product-types=disney-incredi-pass,disney-sorcerer-pass,disney-pirate-pass,disney-pixie-dust-pass&destinationId=WDW&numMonths=13";
+/* fetches and sorts data into arrays for processing */
 
 function getResortData(url, pass, park, parkDate) {
 	fetch(url) /* grab array from disney site */
 		.then((response) => response.json())
 		.then((result) => {
-			var weekArray = []
-			var dayArray = []
-			listLength = 7
-				for (let i = 0; i< listLength; i++){
-					itemDate = new Date();
-					itemDate.setDate(itemDate.getDate() +i);
-					itemDate = itemDate.toISOString().split('T')[0];
-					
-					emptyList = []
-					dateFill = new dateObject(itemDate, emptyList)
-					
-					weekArray.push(dateFill)
-				}
-			
+			var weekArray = [];
+			var dayArray = [];
+			listLength = 7;
+			for (let i = 0; i < listLength; i++) {
+				itemDate = new Date();
+				itemDate.setDate(itemDate.getDate() + i);
+				itemDate = itemDate.toISOString().split("T")[0];
+
+				emptyList = [];
+				dateFill = new dateObject(itemDate, emptyList);
+
+				weekArray.push(dateFill);
+			}
+
 			for (const key in result) {
 				currentPass =
 					result[key]
@@ -99,117 +107,112 @@ function getResortData(url, pass, park, parkDate) {
 					currentPass = result[key].availabilities;
 					for (const date in currentPass) {
 						/*iterate through dates to find the selected date*/
-						if (park !== null){
-						switch (park) {
-							case "ANY" /* will return all available parks for the day at selected resort, but only displays last available park in card*/:
-								if (currentPass[date].date == parkDate) {
-									const matchObject = currentPass[date];
-									cardNotification(matchObject, pass);
-								}
-							default: /*returns availability for selected park only*/
-								if (
-									currentPass[date].date == parkDate &&
-									currentPass[date].facilityId == park
-								) {
-									const matchObject = currentPass[date];
-									cardNotification(matchObject, pass);
-								}
+						if (park !== null) {
+							switch (park) {
+								case "ANY" /* will return all available parks for the day at selected resort, but only displays last available park in card*/:
+									if (currentPass[date].date == parkDate) {
+										const matchObject = currentPass[date];
+										cardNotification(matchObject, pass);
+									}
+								default: /*returns availability for selected park only*/
+									if (
+										currentPass[date].date == parkDate &&
+										currentPass[date].facilityId == park
+									) {
+										const matchObject = currentPass[date];
+										cardNotification(matchObject, pass);
+									}
 							}
-						}else if (date < 28){
+						} else if (date < 28) {
+							/* this function drives the weekly calendar on the right third. */
 							weekPark = currentPass[date].facilityId;
-							weekAvailable = currentPass[date].slots[0].available
-							weekReason = currentPass[date].slots[0].unavailableReason
+							weekAvailable = currentPass[date].slots[0].available;
+							weekReason = currentPass[date].slots[0].unavailableReason;
 							weekAvail = new parkObject(weekPark, weekAvailable, weekReason);
-							for (element in weekArray){
-								if (weekArray[element].date == currentPass[date].date){
+							for (element in weekArray) {
+								if (weekArray[element].date == currentPass[date].date) {
 									weekPush = weekArray[element].slot;
 									weekPush.push(weekAvail);
 								}
 							}
-								
-							}
-							
-							/*for (weekDate in weekArray){
-								console.log(weekArray[weekDate].date, currentPass[date].date)
-								if (weekArray[weekDate].date == currentPass[date].date){
-									console.log("testing week date")
-									dayArray = weekArray[weekDate].slots;
-									dayArray.push(weekPark);
-								}else{
-									dateSlot = new parkObject(weekPark, weekAvailable, weekReason)
-									newDatePush = new dateObject(currentPass[date].date, dateSlot)
-									console.log(newDatePush)
-									weekArray.push(newDatePush)
-									
-								}
-							}*/
 						}
-						
 					}
-					
-				
+				}
 			}
-			displayWeek(weekArray);
-});
+		});
 }
-function displayWeek(weekArray){
-	console.log(weekArray)
-	
-	var currentNode = document.getElementById("displayWeek")
-	var currentElement = document.createElement("br")
-	for (element in weekArray){
-		console.log(weekArray[element].date)
-		var displayDate = weekArray[element].date
-		text = document.createTextNode(`${displayDate}`)
-		currentNode.appendChild(text)
-		currentNode.appendChild(document.createElement("br"))
+
+/* function to display weekly data on the right third*/
+function displayWeek(weekArray) {
+	var currentNode = document.getElementById("displayWeek");
+	var currentElement = document.createElement("br");
+	for (element in weekArray) {
+		console.log(weekArray[element].date);
+		var displayDate = weekArray[element].date;
+		text = document.createTextNode(`${displayDate}`);
+		currentNode.appendChild(document.createElement("br"));
+		currentNode.appendChild(text);
+		currentNode.appendChild(document.createElement("br"));
+		currentNode.appendChild(document.createElement("br"));
 		parkArray = weekArray[element].slot;
-		for (iterPark in parkArray){
+		for (iterPark in parkArray) {
 			var displayPark = parkArray[iterPark].park;
-			var displayAvailable = parkArray[iterPark].available
-			var displayReason = parkArray[iterPark].reason
-			text = document.createTextNode(`${displayPark}, ${displayAvailable}, ${displayReason}`)
+			var displayAvailable = parkArray[iterPark].available;
+			var displayReason = parkArray[iterPark].reason;
+			displayPark = switchParkName(displayPark);
+			console.log(displayReason);
+			switch (displayReason) {
+				case "BLOCKED":
+					textReason = "BLOCKED OUT";
+					break;
+
+				case "NO_INV":
+					textReason = "SOLD OUT";
+					break;
+
+				default:
+					textReason = "AVAILABLE";
+					break;
+			}
+
+			text = document.createTextNode(`${displayPark} ${textReason}`);
 			currentNode.appendChild(text);
-			currentNode.appendChild(document.createElement("br"))
+			currentNode.appendChild(document.createElement("br"));
 		}
-
 	}
-
 }
-function dateObject(date, slot){
+/* constructor function for outer array element, listed by date */
+function dateObject(date, slot) {
 	this.date = date;
 	this.slot = slot;
 }
-
-function parkObject(weekPark, weekAvailable, weekReason ){
+/* constructor function for inner array element, aggregating park data by date for the week */
+function parkObject(weekPark, weekAvailable, weekReason) {
 	this.park = weekPark;
 	this.available = weekAvailable;
 	this.reason = weekReason;
 }
 
-function weekCalendar(){
+function weekCalendar() {
 	weekResort = document.querySelector("#weekSelectResort").value;
-	document.getElementById('displayWeek').innerHTML = ""
+	document.getElementById("displayWeek").innerHTML = "";
 
 	if (weekResort == "DLR") {
 		weekPass = document.querySelector("#weekSelectDLRkey").value;
-		weekPark = null
-		weekDate = null
+		weekPark = null;
+		weekDate = null;
 		weekUrl = dlrUrl;
 	} else if (weekResort == "WDW") {
 		weekPass = document.querySelector("#weekSelectWDWpass").value;
-		weekPark = null
-		weekDate = null
+		weekPark = null;
+		weekDate = null;
 		weekUrl = wdwUrl;
 	}
 	parkDate = document.querySelector("#date").value;
 
-
 	getResortData(weekUrl, weekPass, weekPark, weekDate);
 }
-	
-
-
+/* this function creates the middle third card with notification data */
 function cardNotification(notificationObject, pass) {
 	notifPark = notificationObject.facilityId;
 	notifDate = notificationObject.date;
@@ -219,29 +222,7 @@ function cardNotification(notificationObject, pass) {
 	var passText = "";
 	var reasonText = "";
 
-	switch (notifPark) {
-		case "DLR_DP":
-			parkText = "Disneyland";
-			break;
-		case "DLR_CA":
-			parkText = "California Adventure";
-			break;
-		case "WDW_MK":
-			parkText = "Magic Kingdom";
-			break;
-		case "WDW_MK":
-			parkText = "Magic Kingdom";
-			break;
-		case "WDW_HS":
-			parkText = "Hollywood Studios";
-			break;
-		case "WDW_EP":
-			parkText = "EPCOT";
-			break;
-		case "WDW_AK":
-			parkText = "Animal Kingdom";
-			break;
-	}
+	notifPark = switchParkName(notifPark);
 	switch (pass) {
 		case "inspire-key-pass":
 			passText = "Inspire keys";
@@ -288,16 +269,44 @@ function cardNotification(notificationObject, pass) {
 		notificationObject.facilityId;
 		document.getElementById(
 			"card-text"
-		).innerHTML = `Reservations are available for ${parkText} on ${notifDate} for ${passText}`;
+		).innerHTML = `Reservations are available for ${notifPark} on ${notifDate} for ${passText}`;
 		document.getElementById("card-title").innerHTML = "Come on Down!";
 	} else {
 		reason = notificationObject.slots[0].unavailableReason;
 		document.getElementById(
 			"card-text"
-		).innerHTML = `Reservations are not available for ${parkText} on ${parkDate} because ${reasonText}`;
+		).innerHTML = `Reservations are not available for ${notifPark} on ${parkDate} because ${reasonText}`;
 		document.getElementById("card-title").innerHTML = titleText;
 		document.getElementById("card-button").innerHTML = `Get Lost!`;
 	}
+}
+
+/* function to take park codes and match them with the appropriate string*/
+function switchParkName(parkName) {
+	switch (parkName) {
+		case "DLR_DP":
+			parkText = "Disneyland";
+			break;
+		case "DLR_CA":
+			parkText = "California Adventure";
+			break;
+		case "WDW_MK":
+			parkText = "Magic Kingdom";
+			break;
+		case "WDW_MK":
+			parkText = "Magic Kingdom";
+			break;
+		case "WDW_HS":
+			parkText = "Hollywood Studios";
+			break;
+		case "WDW_EP":
+			parkText = "EPCOT";
+			break;
+		case "WDW_AK":
+			parkText = "Animal Kingdom";
+			break;
+	}
+	return parkText;
 }
 
 /* if (currentPass[date].slots[0].available) {
