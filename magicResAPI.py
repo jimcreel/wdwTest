aa4814d2b3cc1c730ff217ffdce93a3effbb92a1
jimcreel <jim.creel@gmail.com>
2023-01-13 17:35:39 -0800

    new api to serve resort avail in magicResAPI

--- magicResAPI.py
+++ magicResAPI.py
@@ -1,26 +1,32 @@
 import requests
 import json
-import os
-import sys
+from flask import Flask
+from flask_restful import Api, Resource
+app = Flask(__name__)
+api = Api(app)
 
-def main(resort):
-    # determine the resort 
 
-    if(resort == "DLR"):
-        url = os.environ.get('DLR_URL')
-    elif(resort == 'WDW'):
-        url = os.environ.get('WDW_URL')
-    else: return False
-    
-    #get the api key
+class DLR(Resource):
+           
+        def get(userResort: str):
+            url = 'https://disneyland.disney.go.com/passes/blockout-dates/api/get-availability/?product-types=inspire-key-pass,believe-key-pass,enchant-key-pass,imagine-key-pass,dream-key-pass&destinationId=DLR&numMonths=14'
+            data = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"})
+            dataText = data.text
+            dataParsed = json.loads(dataText)
+            
+            return dataParsed, 200
+            
+            
+class WDW(Resource):
+           
+        def get(userResort: str):
+            url = 'https://disneyworld.disney.go.com/passes/blockout-dates/api/get-availability/?product-types=disney-incredi-pass,disney-sorcerer-pass,disney-pirate-pass,disney-pixie-dust-pass&destinationId=WDW&numMonths=13'
+            data = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"})
+            dataText = data.text
+            dataParsed = json.loads(dataText)    
+            return dataParsed, 200
 
-    #open the site
-    print('opening the reservation site...')
-    resp=requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"})
-    #print(resp)
-    dates_dict = resp.text
-    parse_json = json.loads(dates_dict)
-    print(parse_json)
-    return parse_json
-
-main()
+api.add_resource(DLR, "/DLR")
+api.add_resource(WDW, "/WDW")
+if __name__ == '__main__':
+    app.run(debug=True)
